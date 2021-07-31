29dc6e258d31f201055328cdb9411e9395271802
Benjamin CloudoLife <benjamincloudolife@gmail.com>
2021-07-31 09:21:23 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
 ansible-galaxy install -r requirements.yml && ansible-galaxy collection install -r requirements.yml
+
 # ansible-galaxy install -r requirements.yml --force && ansible-galaxy collection install -r requirements.yml --force
